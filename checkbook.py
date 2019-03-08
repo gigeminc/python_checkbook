11256ce592af6be5208d4050a45a831e3a0cda9d
GaryGonzenbach <gonzenbach.gary@gmail.com>
2019-03-08 15:19:02 -0600

    finally got append to work

--- checkbook.py
+++ checkbook.py
@@ -1,29 +1,13 @@
-# setup user input loop
-# ----------  write a dummy file to start off with
+# initial read of checkbook file
 import json
-filename = "Codeup_checkbook.txt"
-checkbook_lst = [
-    {
-        'transaction': 'append test2',
-        'category': 'giraffe',
-        'amount': 150.34
-    },
-]
-with open(filename, 'a', encoding='utf-8') as file:
-    for item in checkbook_lst:
-        x = json.dumps(item, indent=4)
-        file.write(x + '\n')
-#  now read it back
-with open(filename, "r") as read_file:
-    transactions = json.load(read_file)
-    for x in transactions:
-         transamount = transactions['amount']
-         action = transaction['category']
-         print(transamount, action)
-
-
-
-
+filename = "Codeup_checkbook.json"
+# read the entire current checkbook file into a dictionary
+data = json.load(open(filename))
+# -----------------------------------------
+def append_dict(transaction):
+    data.append(transaction)
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file, sort_keys = True, indent = 4)
 # ---------------------------------
 def get_balance():
     current_balance = 1.4
@@ -31,13 +15,19 @@ def get_balance():
     return current_balance
 # ----------------------------------
 def withdraw_balance():
-    current_balance = 1.4
-    print('calling withdraw_balance')
+#    some code to build the dictionary
+#     like this   transaction = {'transaction': 'deposit', 'category': 'open account', 'amount':25.00 }
+    transaction = {'transaction': 'withdraw', 'category': 'need to fill in', 'amount': -25.00 }
+    append_dict(transaction)
+    current_balance = 25
     return current_balance
 # ------------------------------------
 def deposit_balance():
-    current_balance = 1.4
-    print('calling deposit_balance')
+#    some code to build the dictionary
+#     like this   transaction = {'transaction': 'deposit', 'category': 'open account', 'amount':25.00 }
+    transaction = {'transaction': 'deposit', 'category': 'need to fill in', 'amount': 25.00 }
+    append_dict(transaction)
+    current_balance = 25
     return current_balance
 
 # -------------------------------------
@@ -68,35 +58,3 @@ while user_choice != 4:
             user_choice = 0
 
 print('terminal checkbook closed')
-# -------------------------------------
-#    create a book dictionary
-
-checkbook = [
-    {
-        'transaction': 'withdraw',
-        'category': 'entertainment'
-        'amount': 0,
-    },
-    {
-        'transaction': 'deposit',
-        'category': 'paycheck'
-        'amount': 0,
-    },
-    {
-        'transaction': 'inquiry',
-        'category': ''
-        'amount': 0,
-    },
-]
-
-
-
-# genre_to_show = input('Enter a genre: ')
-# for book in books:
-#    if genre_to_show not in book['genre']:
-#        continue
- 
-#    print('---------------')        
-#    print('- title: %s' % book['title'])    
-#    print('- author: %s' % book['author'])
-#    print('- genre: %s' % book['genre'])
